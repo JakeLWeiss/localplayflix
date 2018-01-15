7a6db754512feb803b8860ea6abcf054a6850eac
Jake Weiss <32331702+JakeLWeiss@users.noreply.github.com>
2018-01-15 11:50:55 -0500

    menu now loads from json file on apache server :D yay for limitless scaling! we should probably add a search function tho...ugh

--- mov/menu/MainPage.xaml.cs
+++ mov/menu/MainPage.xaml.cs
@@ -1,39 +1,36 @@
 ﻿using Binding;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Net;
+using System.Net.Http;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
-namespace menu
-{
+namespace menu {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
         private List<Movie> movies;
+
         public MainPage() {
             this.InitializeComponent();
             movies = MoviesManager.GetMovies();
         }
+
         private void GridView_ItemClick(object sender, ItemClickEventArgs e) {
             var movie= (Movie)e.ClickedItem;
-            ResultTextBlock.Text = "You Selected--->>" + movie.name + "--->>description_ " + movie.description;
+            ResultTextBlock.Text = "You Selected--->>" + movie.name + " with URI " + movie.id;
         }
+
+       
     }
 }
+
 
 namespace Binding {
     public class Movie {
@@ -45,7 +42,7 @@ namespace Binding {
             get;
             set;
         }
-        public string id {
+        public Uri id {
             get;
             set;
         }
@@ -53,26 +50,36 @@ namespace Binding {
             get;
             set;
         }
+
     }
+}
+
     public class MoviesManager {
+
         public static List<Movie> GetMovies() {
             var movies = new List<Movie>();
-            movies.Add(new Movie {
-                name = "1", description = "was actually gonna use dustins fb profile but meh", thumbnail = "https://vignette.wikia.nocookie.net/inazuma-eleven/images/4/48/Tenma_and_Shuu_in_GO_Movie_HQ.PNG/revision/latest?cb=20160715140309"
-            });
-            movies.Add(new Movie {
-                name = "2", description = "description of movie", thumbnail = "https://vignette.wikia.nocookie.net/inazuma-eleven/images/4/48/Tenma_and_Shuu_in_GO_Movie_HQ.PNG/revision/latest?cb=20160715140309"
-            });
-            movies.Add(new Movie {
-                name = "3", description = "description of movie", thumbnail = "https://vignette.wikia.nocookie.net/inazuma-eleven/images/4/48/Tenma_and_Shuu_in_GO_Movie_HQ.PNG/revision/latest?cb=20160715140309"
-            });
-            movies.Add(new Movie {
-                name = "4", description = "description of movie", thumbnail = "https://vignette.wikia.nocookie.net/inazuma-eleven/images/4/48/Tenma_and_Shuu_in_GO_Movie_HQ.PNG/revision/latest?cb=20160715140309"
-            });
-            movies.Add(new Movie {
-                name = "5", description = "description of movie", thumbnail = "https://vignette.wikia.nocookie.net/inazuma-eleven/images/4/48/Tenma_and_Shuu_in_GO_Movie_HQ.PNG/revision/latest?cb=20160715140309"
-            });
+            
+            List<Movie> movs = new List<Movie>();
+            try {
+                using (HttpClient client = new HttpClient()) {
+                    using (HttpResponseMessage response = client.GetAsync("http://192.168.0.137/metadatafull.json").Result) {
+                        using (HttpContent content = response.Content) {
+                            var json = content.ReadAsStringAsync().Result;
+                            List<JObject> jobs = JsonConvert.DeserializeObject<List<JObject>>(json);
+                            foreach (JObject j in jobs) { 
+                                movies.Add(new Movie { 
+                                    id = new Uri((string)j.GetValue("movieid")), //set parameters to watch the movie with
+                                    name = (string)j.GetValue("description"),
+                                    description = (string)j.GetValue("description"),
+                                    thumbnail = (string)j.GetValue("thumbnail"),
+                                });
+                            }
+                        }
+                    }
+                }
+            } catch {
+                Console.WriteLine("Connection to apache server failed. Test media loaded");
+            }        
             return movies;
         }
     }
-}
