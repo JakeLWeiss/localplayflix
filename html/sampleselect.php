e6c8e6c5b49ae65bbfda6b0a253889557115fac0
Unknown <duslm2001@gmail.com>
2018-01-22 21:56:47 -0500

    for real this time
    the sampleselect actually depends on the submission. dont forget to update the database and the mysql users.

--- html/sampleselect.php
+++ html/sampleselect.php
@@ -1,11 +1,10 @@
 <?php
 include 'networking.php';
 
-
 $conn = new databaseConnection;
 $conn->connect();
 
-$result = $conn->pdo->query("select * from movies where name like 'Mirai%'")->fetch();
+$result = $conn->pdo->query("select * from movies where name like '%" . $_POST['name'] . "%'")->fetch();
 
 print_r($result);
 ?> 
